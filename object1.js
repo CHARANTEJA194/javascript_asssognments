53688d4f64977fbfa2de333c1ed316c3fc683883
CHARANTEJA194 <166363605+CHARANTEJA194@users.noreply.github.com>
2024-06-13 10:07:24 +0300

    Create object1.js

---
+++ object1.js
@@ -0,0 +1,12 @@
+
+var anime=
+{
+    title : "Naruto",
+    director:"kisimoto",
+    yearPublished: 2001,
+    printanimedata: function()
+    {
+    console.log("the anime titled"+this.title+"is anime by"+this.director);
+    }
+};
+console.log(anime.printanimedata());
